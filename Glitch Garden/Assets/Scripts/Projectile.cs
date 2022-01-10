76bab3db9bbfaf5036ccd8e8d43d002da031498a
AdemArchS <ademmalone@gmail.com>
2022-01-10 13:23:17 -0600

    GlitchGarden

---
+++ Glitch Garden/Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] float speed = 2f;
+    [SerializeField] float damage = 50f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * Time.deltaTime * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        //reduce health
+        var health = otherCollider.GetComponent<Health>();
+        var attacker = otherCollider.GetComponent<Attacker>();
+
+        if(attacker && health)
+        {
+            health.DealDamage(damage);
+            Destroy(gameObject);
+        }
+        
+    }
+}
